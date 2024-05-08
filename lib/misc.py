85d7600ad1b7ca051388099153a2de341ea9754c
Yuanlong Li <yuanlong.li@epfl.ch>
2024-05-08 11:29:32 +0000

    update

--- lib/misc.py
+++ lib/misc.py
@@ -13,14 +13,20 @@ def prep(src):
 
 
 def copy(src, dst):
-    shutil.move(os.path.join(src, 'adv'),               dst)
-    shutil.move(os.path.join(src, 'trace.log'),         dst)
-    shutil.move(os.path.join(src, 'trace.bin'),         dst)
-    shutil.move(os.path.join(src, 'stats_db.out.gz'),   os.path.join(dst, 'trace.gz'))
-    shutil.move(os.path.join(src, 'configuration.out'), os.path.join(dst, 'timing.cfg'))
+    def move(src, dst):
+        try:
+            shutil.move(src, dst)
+        except:
+            pass
 
-    shutil.move(os.path.join(dsc, 'stats_db.out.gz'),   os.path.join(dst, 'timing.gz'))
-    shutil.move(os.path.join(dsc, 'configuration.out'), os.path.join(dst, 'timing.cfg'))
+    move(os.path.join(src, 'adv'),                      dst)
+    move(os.path.join(src, 'trace.log'),                dst)
+    move(os.path.join(src, 'trace.bin'),                dst)
+    move(os.path.join(src, 'stats_db.out.gz'),          os.path.join(dst, 'trace.gz'))
+    move(os.path.join(src, 'configuration.out'),        os.path.join(dst, 'trace.cfg'))
+
+    move(os.path.join(dst, 'adv', 'stats_db.out.gz'),   os.path.join(dst, 'timing.gz'))
+    move(os.path.join(dst, 'adv', 'configuration.out'), os.path.join(dst, 'timing.cfg'))
 
 
 def para(file, info, core, *cmds):
@@ -34,7 +40,7 @@ def para(file, info, core, *cmds):
             '--max-procs', str(core)
         ],
         shell = False,
-        check = True,
+        check = False,
         env = {
             'PATH': os.environ['PATH'],
             'HOME': os.environ['HOME'],
